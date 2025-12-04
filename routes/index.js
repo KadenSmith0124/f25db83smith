dd0ecd858d302e2be5c0a593ed745cca537f6208
Kaden Smith <kaden.smith0124@gmail.com>
2025-12-03 21:27:07 -0600

    Added steps 3 and 4

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,72 @@
 var express = require('express');
+var passport = require('passport');
 var router = express.Router();
+var Account = require('../models/account');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+// INDEX PAGE
+router.get('/', function (req, res) {
+  res.render('index', { title: 'Spell App', user: req.user });
+});
+
+// REGISTER PAGE
+router.get('/register', function(req, res) {
+  res.render('register', { title: 'Spell App Registration' });
+});
+
+// REGISTER POST
+router.post('/register', function(req, res) {
+  Account.findOne({ username: req.body.username })
+    .then(function(existing) {
+      if (existing) {
+        return res.render('register', {
+          title: 'Registration',
+          message: 'Existing User',
+          account: req.body.username
+        });
+      }
+
+      let newAccount = new Account({ username: req.body.username });
+
+      Account.register(newAccount, req.body.password, function(err, user) {
+        if (err || !user) {
+          return res.render('register', {
+            title: 'Registration',
+            message: 'access error',
+            account: req.body.username
+          });
+        }
+
+        res.redirect('/');
+      });
+    });
+});
+
+// LOGIN PAGE
+router.get('/login', function(req, res) {
+  res.render('login', { title: 'Spell App Login', user: req.user });
+});
+
+// LOGIN POST
+router.post('/login', passport.authenticate('local'), function(req, res) {
+  if (req.session.toReturn) {
+    let toReturn = req.session.toReturn;
+    req.session.toReturn = null;
+    return res.redirect(toReturn);
+  }
+  res.redirect('/');
+});
+
+// LOGOUT
+router.get('/logout', function(req, res) {
+  req.logout(function(err) {
+    if (err) { return next(err); }
+    res.redirect('/');
+  });
+});
+
+// PING (test endpoint)
+router.get('/ping', function(req, res) {
+  res.status(200).send("pong!");
 });
 
 module.exports = router;
--- routes/spells.js
+++ routes/spells.js
@@ -2,15 +2,30 @@ var express = require('express');
 const spell_controllers = require('../controllers/spell');
 var router = express.Router();
 
+// Authentication check function
+// Redirects to login if user is not authenticated
+const secured = (req, res, next) => {
+  if (req.user) {
+    return next();
+  }
+  // Remember where user was going
+  req.session.toReturn = req.originalUrl;
+  res.redirect("/login");
+};
+
 /* GET Spells page. */
 router.get('/', spell_controllers.spell_view_all_Page);
+
 /* GET detail spell page */
 router.get('/detail', spell_controllers.spell_view_one_Page);
-/* GET create Spell page */
+
+/* GET create spell page */
 router.get('/create', spell_controllers.spell_create_Page);
-/* GET Spell update page */
-router.get('/update', spell_controllers.spell_update_Page);
-/* GET Spell delete page */
+
+/* GET spell update page (PROTECTED) */
+router.get('/update', secured, spell_controllers.spell_update_Page);
+
+/* GET spell delete page */
 router.get('/delete', spell_controllers.spell_delete_Page);
 
 module.exports = router;
